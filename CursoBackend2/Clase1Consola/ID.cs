d5d5dc3f6e9ea37378d150625ada357e0440d9bc
juansubas <juansubas@gmail.com>
2024-04-12 18:18:04 -0500

    Curso Backend Inyeccion Dependencias

---
+++ CursoBackend2/Clase1Consola/ID.cs
@@ -0,0 +1,15 @@
+﻿namespace Clase1Consola;
+
+public interface IID
+{
+    Guid Value { get; }
+}
+
+public interface IIDSingleton : IID { }
+public interface IIDScoped : IID { }
+public interface IIDTrasient : IID { }
+
+public class ID : IIDSingleton, IIDScoped, IIDTrasient
+{
+    public Guid Value { get; private set; } = Guid.NewGuid();
+}
---
+++ CursoBackend2/Clase1Consola/Program.cs
@@ -0,0 +1,22 @@
+﻿using Clase1Consola;
+using Microsoft.Extensions.DependencyInjection;
+
+var service = new ServiceCollection();
+service.AddSingleton<IIDSingleton>(new ID());
+service.AddScoped<IIDScoped, ID>();
+service.AddTransient<IIDTrasient, ID>();
+
+
+var app = service.BuildServiceProvider();
+var single = app.GetRequiredService<IIDSingleton>();
+var scoped1 = app.GetRequiredService<IIDScoped>();
+var scoped2 = app.GetRequiredService<IIDScoped>();
+var trasient1 = app.GetRequiredService<IIDTrasient>();
+var trasient2 = app.GetRequiredService<IIDTrasient>();
+
+Console.WriteLine($"Singleton {single.Value}");
+
+Console.WriteLine($"Scoped1 {scoped1.Value}");
+Console.WriteLine($"Scoped2 {scoped2.Value}");
+Console.WriteLine($"Trasient1 {trasient1.Value}");
+Console.WriteLine($"Trasient2 {trasient2.Value}");
