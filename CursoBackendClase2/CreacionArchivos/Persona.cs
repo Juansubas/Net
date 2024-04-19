7e658afcc3f531976cab3eb77e16e0380bd525f6
juansubas <juansubas@gmail.com>
2024-04-19 18:39:53 -0500

    Creando Lectura mapeado y guardando los datos procesados en otra ruta.

---
+++ CursoBackendClase2/CreacionArchivos/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace CreacionArchivos;
+
+internal class Persona
+{
+    public int Id { get; set; }
+    public string First_name { get; set; }
+    public string Last_name { get; set; }
+    public string Email { get; set; }
+    public string Gender { get; set; }
+    public string Ip_address { get; set; }
+    public string Company_name { get; set; }
+
+}
--- CursoBackendClase2/CreacionArchivos/Program.cs
+++ CursoBackendClase2/CreacionArchivos/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using CreacionArchivos;
+using System.ComponentModel;
+using System.IO;
 
 //string directorio = "c:/lib/archivosCreados/";
 //string archivoACrear = "primerArchivo.txt";
@@ -46,3 +48,41 @@
 //    Console.WriteLine("Se ha generado un error al crear el archivo", ex.Message);
 //    throw;
 //}
+
+
+//Bloque 3 
+
+// leer Contenido archivos
+
+string rutaArchivo = @"C:\Users\juans\Downloads\ArchivosCreados\MOCK_DATA.csv";
+string rutaProcesado = $"{Guid.NewGuid()}.csv";
+List<Persona> lista = new List<Persona>();
+
+if (File.Exists(rutaArchivo))
+{
+    string[] lineas = File.ReadAllLines(rutaArchivo);
+    foreach (string linea in lineas.Skip(1))
+    {
+        string[] lineaPersona = linea.Split(",");
+        var persona = new Persona()
+        {
+            Id = Convert.ToInt32(lineaPersona[0]),
+            First_name = lineaPersona[1],
+            Last_name = lineaPersona[2],
+            Email = lineaPersona[3],
+            Gender = lineaPersona[4],
+            Ip_address = lineaPersona[5],
+            Company_name = lineaPersona[6],
+        };
+
+        lista.Add(persona);
+
+    }
+
+    if (lista.Count > 0)
+    {
+        File.Move(rutaArchivo, Path.Combine(Path.GetDirectoryName(rutaArchivo), rutaProcesado));
+    }
+
+    Console.WriteLine($"Cantidad de personas {lista.Count()}");
+}
