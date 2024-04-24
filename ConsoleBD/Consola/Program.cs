4aebece243160df42db6b71c9ae5ef05abca0c10
juansubas <juansubas@gmail.com>
2024-04-24 03:00:39 -0500

    Consulta a Usuarios y lectura

--- ConsoleBD/Consola/Program.cs
+++ ConsoleBD/Consola/Program.cs
@@ -2,27 +2,30 @@
 
 using Microsoft.Data.SqlClient;
 
-var Servidor = string.Empty;
-var Bd = string.Empty;
-var Usuario = string.Empty;
-var Clave = string.Empty;
+//var Servidor = string.Empty;
+//var Bd = string.Empty;
+//var Usuario = string.Empty;
+//var Clave = string.Empty;
 
-Console.WriteLine("Por favor ingrese el servidor de base de datos");
-Servidor = Console.ReadLine();
-Console.WriteLine("Por favor ingrese la bd: ");
-Bd = Console.ReadLine();
+//Console.WriteLine("Por favor ingrese el servidor de base de datos");
+//Servidor = Console.ReadLine();
+//Console.WriteLine("Por favor ingrese la bd: ");
+//Bd = Console.ReadLine();
 //Console.WriteLine("Por favor ingrese el usuario: ");
 //Usuario = Console.ReadLine();
 //Console.WriteLine("Por favor ingrese la clave: ");
 //Clave = Console.ReadLine();
 
 //;User Id ={Usuario};Password={Clave}
-var cadena = $"Server={Servidor};Database={Bd};Integrated Security=True;TrustServerCertificate=True";
+//var cadena = $"Server={Servidor};Database={Bd};Integrated Security=True;TrustServerCertificate=True";
+var cadena = $"Server=.;Database=APIColombia;Integrated Security=True;TrustServerCertificate=True";
 Console.WriteLine($"La cadena de conexion {cadena}");
 
 using (SqlConnection conn = new SqlConnection(cadena))
 {
-    var query = "SELECT @@SERVERNAME";
+	// var query = "SELECT @@SERVERNAME"; // Consultar nombre del Servidor
+
+	var query = "SELECT * FROM Usuarios";
 
     SqlCommand cmd = new SqlCommand(query, conn);
 
@@ -32,7 +35,8 @@ using (SqlConnection conn = new SqlConnection(cadena))
 		SqlDataReader reader = cmd.ExecuteReader();
 		while (reader.Read())
 		{
-			Console.WriteLine($"Nombre del servidor: {reader[0]}");
+			//Console.WriteLine($"Nombre del servidor: {reader[0]}");
+			Console.WriteLine($"Id: {reader["Id"]}, Nombre: {reader["Nombre"]}, Apellido: {reader["Apellido"]}");
 		}
 	}
 	catch (Exception ex)
