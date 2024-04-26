b00fd7b152101808eab9d711bf35a3bc278e238e
juansubas <juansubas@gmail.com>
2024-04-26 16:44:05 -0500

    App FUncional enviando correos, con arquitectura y simulando workerservice

---
+++ DMS.IntegracionesCurso/DMS.IntegracionesCurso.App/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using DMS.IntegracionesCurso.Core.Interfaces;
+using DMS.IntegracionesCurso.Infrastructure.Repositories;
+using DMS.IntegracionesCurso.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var serviceProvider = new ServiceCollection();
+serviceProvider.AddSingleton(configuration);
+serviceProvider.AddTransient<IArchivoService, ArchivoService>();
+serviceProvider.AddTransient<ICorreoService, CorreoService>();
+serviceProvider.AddTransient<ICursoRepository, CursoRepository>();
+serviceProvider.AddTransient<ILogService, LogService>();
+serviceProvider.AddTransient<IExecuteService, ExecuteService>();
+
+var app = serviceProvider.BuildServiceProvider();
+var execute = app.GetService<IExecuteService>();
+execute.Execute();
